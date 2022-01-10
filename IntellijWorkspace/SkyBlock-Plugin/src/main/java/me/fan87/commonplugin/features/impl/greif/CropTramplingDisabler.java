e6ec0c7113904277783129b4a595b62a62f2376b
fan87 <fan87.TW@gmail.com>
2022-01-10 20:44:09 +0800

    Fuck my life

--- IntellijWorkspace/SkyBlock-Plugin/src/main/java/me/fan87/commonplugin/features/impl/greif/CropTramplingDisabler.java
+++ IntellijWorkspace/SkyBlock-Plugin/src/main/java/me/fan87/commonplugin/features/impl/greif/CropTramplingDisabler.java
@@ -2,7 +2,9 @@ package me.fan87.commonplugin.features.impl.greif;
 
 import me.fan87.commonplugin.features.SBFeature;
 import org.bukkit.Material;
+import org.bukkit.entity.Creature;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.greenrobot.eventbus.Subscribe;
 
@@ -20,10 +22,20 @@ public class CropTramplingDisabler extends SBFeature {
     protected void onDisable() {
 
     }
+
     @Subscribe
     public void onCropDestroy(PlayerInteractEvent event) {
 
         if (event.getAction() == Action.PHYSICAL && event.getClickedBlock().getType().equals(Material.SOIL)) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    @Subscribe
+    public void onCropDestroy(EntityInteractEvent event) {
+
+        if (event.getEntity() instanceof Creature && event.getBlock().getType().equals(Material.SOIL)) {
             event.setCancelled(true);
         }
 
